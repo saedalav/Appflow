404af3dba1c7b159e48ccb58d68fa4f14d30c57c
Saed <saed.a@live.ca>
2016-07-18 16:27:31 -0400

    After Revert

--- AppFlowWeb/src/appFlow/listeners/ApplicationStartup.java
+++ AppFlowWeb/src/appFlow/listeners/ApplicationStartup.java
@@ -13,7 +13,7 @@ import org.activiti.engine.ProcessEngines;
 public class ApplicationStartup implements ServletContextListener {
 
 	//Make sure JDBC Driver for your Database is on the path
-	//Set the following Variables
+	//Set the following Variables.
 	final private String jdbcUrl = "jdbc:mysql://localhost:3306/appflowdb?autoReconnect=true";
 	final private String jdbcDriver = "com.mysql.jdbc.Driver";
 	final private String jdbcUser = "root";
